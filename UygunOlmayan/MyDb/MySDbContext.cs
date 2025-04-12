2d9bf30e8d0156ef894e369f808817b6ee5afa2d
mert karaömer <mert.karaomer@ogr.sakarya.edu.tr>
2025-04-12 10:21:57 +0300

    fix

--- UygunOlmayan/MyDb/MySDbContext.cs
+++ UygunOlmayan/MyDb/MySDbContext.cs
@@ -12,7 +12,7 @@ namespace UygunOlmayan.MyDb
         {
             // Burada veritabanı bağlantı bilgilerini tanımlayın.
             // Örnek olarak SQL Server kullanalım:
-            string connectionString = "Data Source=SRVMIKRO;Initial Catalog=MikroDB_V16_ICM;Integrated Security=True;Connect Timeout=10;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            string connectionString = "Data Source=SRV-MIKRO;Initial Catalog=MikroDB_V16_ICM;Integrated Security=True;Connect Timeout=10;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
             optionsBuilder.UseSqlServer(connectionString);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
